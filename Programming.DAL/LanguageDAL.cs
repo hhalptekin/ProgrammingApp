5c37d9af604c2e9c870c6a85d16b8bcd480a6a5d
HHALPTEKİN <alptekin_-_@hotmail.com>
2019-05-29 14:19:58 +0300

    Add project files.

---
+++ Programming.DAL/LanguageDAL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.DAL
+{
+    public class LanguageDAL
+    {
+        ProgrammingDbEntities db = new ProgrammingDbEntities();
+
+        public IEnumerable<Language> GetAllLanguages()
+        {
+            return db.Language;
+        }
+        public Language GetLanguagesById(int id)
+        {
+            return db.Language.Find(id);
+
+        }
+        public Language CreateLanguages(Language languages)
+        {
+            db.Language.Add(languages);
+            db.SaveChanges();
+            return languages;
+
+        }
+        public Language UpdateLangusages(int id, Language languages)
+        {
+
+            db.Entry(languages).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return languages;
+
+
+        }
+
+        public void DeleteLanguages(int id)
+        {
+
+            db.Language.Remove(db.Language.Find(id));
+            db.SaveChanges();
+        }
+
+    }
+}
---
+++ ProgrammingAPI/Controllers/LanguagesController.cs
@@ -0,0 +1,40 @@
+﻿using Programming.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProgrammingAPI.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        LanguageDAL languageDAL = new LanguageDAL();
+
+        public IEnumerable<Language> Get()
+        {
+            return languageDAL.GetAllLanguages();
+
+        }
+        public Language Get(int id)
+        {
+            return languageDAL.GetLanguagesById(id);
+
+        }
+
+        public Language Post(Language languages)
+        {
+            return languageDAL.CreateLanguages(languages);
+        }
+
+        public Language Put(int id,Language languages)
+        {
+            return languageDAL.UpdateLangusages(id,languages);
+        }
+        public void Delete(int id)
+        {
+             languageDAL.DeleteLanguages(id);
+        }
+    }
+}
